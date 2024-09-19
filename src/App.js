a537d4223ef90fda26c712b448b6fcaeb457968f
sirigiri-software-solutions <159229544+sirigiri-software-solutions@users.noreply.github.com>
2024-09-19 15:52:09 +0530

    Revert "Add google maps button on all buttons"

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import React from "react";
- 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./Components/Login"
-import Signup from "./Components/Signup";
-import Dashboardpage from "./Components/Dashboardpage" 
-import Allhostelsdata from "./Components/Allhostelsdata";
-import Mainadmin from "./Components/Mainadmin";
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
-    <Router>
-     
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup/>}></Route>
- 
-        <Route path="/Dashboardpage" element={<Dashboardpage/>} />
-        <Route path="/allhostelsdata" element={<Allhostelsdata/>} />
-        <Route path="/Mainadmin" element={<Mainadmin/>} />
-      
-        
-
-      </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
+
 export default App;
- 
